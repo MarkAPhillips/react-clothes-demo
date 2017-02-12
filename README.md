fe991c89ed0f4cb8be69cb6f5b917a24252ae19e
Mark Phillips <work-mpconsultants@outlook.com>
2017-02-12 09:00:31 +0000

    Add sinon as promised to handle spys on resource

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# React Sales Demo 
+# React Datagrid Sale Order Demostration 
 
 Master : [![CircleCI](https://circleci.com/gh/MarkAPhillips/react-datagrid-demo/tree/master.svg?style=svg)](https://circleci.com/gh/MarkAPhillips/react-datagrid-demo/tree/master)
 
--- test/setup.js
+++ test/setup.js
@@ -6,6 +6,9 @@ const chaiImmutable = require('chai-immutable');
 
 global.chai.use(chaiImmutable);
 
+/** Required to spy on methods that return promises */
+require('sinon-as-promised');
+
 const jsdom = require('jsdom').jsdom;
 const document = jsdom( '<html/>' );
 const window = document.defaultView;
